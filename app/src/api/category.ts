6c72adffe82a8e139d39b30556d71d5cab59cfa3
hmuri <minju.choi@ringleplus.com>
2024-03-30 16:47:53 +0900

    [feat] category api 연결

--- app/src/api/category.ts
+++ app/src/api/category.ts
@@ -1,17 +1,14 @@
 import axios from "axios";
+import axiosInstance from "./axios";
 
-const API_BASE_URL = "https://hearo-server.shop:8080";
-
-// 검색 결과를 가져오는 함수 
+// 검색 결과를 가져오는 함수
 export const getCategoryItems = async (categoryId: number) => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/item/category/${encodeURIComponent(categoryId)}`);
-        console.log(response.data); 
-        return response.data;
-    }
-    catch (error) {
-        console.error("검색 결과를 가져오는데 실패했습니다.", error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.get(`/outfits/${categoryId}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("검색 결과를 가져오는데 실패했습니다.", error);
+    throw error;
+  }
 };
-
